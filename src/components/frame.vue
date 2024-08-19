99345475654a25ae635c1039f0e59d2f71985680
yxytsuki <2751415916@qq.com>
2024-08-19 12:18:19 +0800

    feat消费记录（日消费记录，月消费记录，年消费记录）

---
+++ src/components/frame.vue
@@ -0,0 +1,22 @@
+<template>
+  <el-container>
+    <el-aside width="20%">
+      <nav-menu />
+    </el-aside>
+  
+    <el-container>
+      <el-header>
+        <head-menu />
+      </el-header>
+      <el-main>
+        <router-view />
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
