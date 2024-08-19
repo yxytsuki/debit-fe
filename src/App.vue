98baa7c7660456452755326072d0e5bcc9c3be0d
yxytsuki <2751415916@qq.com>
2024-08-19 12:53:22 +0800

    App

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+.my-header{
+  background: yellowgreen;
+  width: 100%;
+  height: 100%;
+}
+
+.my-container{
+  background: deeppink;
+  width: 100%;
+  height: 400px;
+}
+</style>
