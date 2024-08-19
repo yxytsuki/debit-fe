4fe7dee08524e8dff230c3ec9bba1db12543539f
yxytsuki <2751415916@qq.com>
2024-08-19 12:47:33 +0800

    style修复eslint格式化问题

---
+++ src/router/Statistics/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="main">
+    <el-row class="row1">
+      <router-link :to="{ name: 'Statistic.Day' }"><el-button type="primary">按日统计</el-button></router-link>
+      <router-link :to="{ name: 'Statistic.Month' }"><el-button type="primary">按月统计</el-button></router-link>
+      <router-link :to="{ name: 'Statistic.Year' }"><el-button type="primary">按年统计</el-button></router-link>
+    </el-row>
+
+    <el-row >
+     <router-view/>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Statistic'
+}
+</script>
+
+<style>
+</style>
